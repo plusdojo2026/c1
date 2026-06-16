01936b5501a49cfd5211b9fb23a25f3f608e091d
plusdojo2026-C-Terada <terada-koto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 14:35:01 +0900

    Update UserDAOTest.java

--- source/src/main/java/test/UserDAOTest.java
+++ source/src/main/java/test/UserDAOTest.java
@@ -1,5 +1,31 @@
 package test;
 
-public class UserDAOTest {
+import dao.UserDao;
+import dto.User;
 
+public class UserDAOTest {
+	public static void main(String[] args) {
+		testIsLoginOK1(); // ユーザーが見つかる場合のテスト
+		testIsLoginOK2(); // ユーザーが見つからない場合のテスト
+	}
+
+	// ユーザーが見つかる場合のテスト
+	public static void testIsLoginOK1() {
+		UserDao dao = new UserDao();
+		if (dao.isLoginOK(new User("yamada-yuki-m","", "001",0))) {
+			System.out.println("testIsLoginOK1：テストが成功しました");
+		} else {
+			System.out.println("testIsLoginOK1：テストが失敗しました");
+		}
+	}
+
+	// ユーザーが見つからない場合のテスト
+	public static void testIsLoginOK2() {
+		UserDao dao = new UserDao();
+		if (!dao.isLoginOK(new User("yamada-yuki-m","", "001",0))) {
+			System.out.println("testIsLoginOK2：テストが成功しました");
+		} else {
+			System.out.println("testIsLoginOK2：テストが失敗しました");
+		}
+	}
 }
