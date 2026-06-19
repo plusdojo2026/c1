0c5b42cfbcdaf44d94d476d1b3039f77fd24cf2c
plusdojo2026-tanaka-yo <tanaka-yoshihito-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:38:54 +0900

    Update HomeServlet.java

--- source/src/main/java/servlet/HomeServlet.java
+++ source/src/main/java/servlet/HomeServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.SuggestionDao;
+import dto.Result;
+import dto.Suggestion;
+
 /**
  * Servlet implementation class HomeServlet
  */
@@ -34,4 +38,27 @@ public class HomeServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Home.jsp");
 		dispatcher.forward(request, response);
 	}
-}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+	
+		String user_id = (String)session.getAttribute("user_id");	
+		String suggestion = request.getParameter("suggestion");	
+		
+		// 登録処理を行う
+		SuggestionDao sDao = new SuggestionDao();
+		if (sDao.insert(new Suggestion(0, "", user_id, suggestion))) { // 送信成功
+				request.setAttribute ("result", new Result("送信成功！", "ご意見を送信しました。", "/c1/HomeServlet"));
+			} else { // 送信失敗
+				request.setAttribute ("result", new Result("送信失敗！", "ご意見を送信できませんでした。", "/c1/HomeServlet"));
+		}
+
+		// ホーム画面にフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/HomeResult.jsp");
+		dispatcher.forward(request, response);
+		}
+	}
