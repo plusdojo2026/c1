3eb5cf20ce32d3e040e5bbf6b5f0c4bfb8fbd35b
plusdojo2026-tanaka-yo <tanaka-yoshihito-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:38:48 +0900

    Update SuggestionDao.java

--- source/src/main/java/dao/SuggestionDao.java
+++ source/src/main/java/dao/SuggestionDao.java
@@ -4,25 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+// 日付や時刻のフォーマットの指定
+import java.text.SimpleDateFormat;
+// 現在の年月日を取得する
+import java.util.Calendar;
 
 import dto.Suggestion;
 
 public class SuggestionDao {
+	
 // 引数cardで指定されたレコードを登録し、成功したらtrueを返す
 	public boolean insert(Suggestion card) {
 	    Connection conn = null;
-//	    boolean result = false;
-	    
-	    try {
-	    	// JDBCドライバを読み込む
-	    	Class.forName("com.mysql.cj.jdbc.Driver");
-	    } catch (ClassNotFoundException e){
-	    	throw new IllegalStateException(
-	    			"JDBCドライバを読み込めませんでした。");
-	    }
+	    boolean result = false;
 	    
 		try {
-			
+			// JDBCドライバを読み込む
+	    	Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
@@ -34,28 +32,62 @@ public class SuggestionDao {
 //						"FROM suggestion INNER JOIN user ON suggestion.user_id = user.user_id "+
 //						"INSERT INTO suggestion VALUES (0, ?, ?)";
 			
+//			日付を取得し変数をを格納
+			Calendar cl = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			
+			
 			// SQL文を準備する(idは自動連番なので指定しなくてよい)
-			String sql = "INSERT INTO suggestion(user_id, suggestion) VALUES(?, ?)";
+			String sql = "INSERT INTO suggestion VALUES(0, ?, ?, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
 			// SQL文の「？」に使用する値を設定してSQL文を完成
-	        pStmt.setString(1, card.getUser_id());
-	        pStmt.setString(2, card.getSuggestion());
+			// pStmt.setString(1, card.getUser_id());
+	        // 日付を格納させる
+			// pStmt.setString(2, sdf.format(cl.getTime()));
+			// pStmt.setString(3, card.getSuggestion());
 	        
-	        // INSERT文を実行(resultには追加された行数が代入される)
-	         int result = pStmt.executeUpdate();
-	        if (result != 1) {
-	        	return false;
-	        }
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
+	     // SQL文を完成させる
+	     			if (card.getUser_id() != null) {
+	     				pStmt.setString(1, card.getUser_id());
+	     			} else {
+	     				pStmt.setString(1, "");
+	     			}
+	     			if (sdf.format(cl.getTime()) != null) {
+	     				pStmt.setString(2, sdf.format(cl.getTime()));
+	     			} else {
+	     				pStmt.setString(2, "");
+	     			}
+	     			if (card.getSuggestion() != null) {
+	     				pStmt.setString(3, card.getSuggestion());
+	     			} else {
+	     				pStmt.setString(3, "");
+	     			}
+
+	     			// SQL文を実行する
+	     			if (pStmt.executeUpdate() == 1) {
+	     				result = true;
+	     			}
+	     		} catch (SQLException e) {
+	     			e.printStackTrace();
+	     		} catch (ClassNotFoundException e) {
+	     			e.printStackTrace();
+	     		} finally {
+	     			// データベースを切断
+	     			if (conn != null) {
+	     				try {
+	     					conn.close();
+	     				} catch (SQLException e) {
+	     					e.printStackTrace();
+	     				}
+	     			}
+	     		}
+
 	        
 	        
 
 		// 結果を返す
-		return true;
+		return result;
 	}
 }
