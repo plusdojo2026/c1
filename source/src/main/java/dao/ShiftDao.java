b6a960d1b24afc192f67365986246ee2b41d5e6b
plusdojo2026-fujisawa <fujisawa-hiroto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 10:58:23 +0900

    shiftdao藤澤が変更

--- source/src/main/java/dao/ShiftDao.java
+++ source/src/main/java/dao/ShiftDao.java
@@ -28,10 +28,11 @@ public class ShiftDao {
 	            "root", "password");
 
 	        String sql =
-	            "SELECT shift.id, user.user_name, shift.date, " +
-	            "shift.clock_in, shift.clock_out, shift.real_in, shift.real_out " +
-	            "FROM shift INNER JOIN user ON shift.user_id = user.user_id " +
-	            "WHERE 1=1";
+	        	    "SELECT shift.id, user.user_id, user.user_name, shift.date, " +
+	        	    "shift.clock_in, shift.clock_out, shift.real_in, shift.real_out " +
+	        	    "FROM shift INNER JOIN user ON shift.user_id = user.user_id " +
+	        	    "WHERE 1=1";
+
 
 	        if (shift.getWord() != null && !shift.getWord().isEmpty()) {
 	            sql += " AND (user.user_name LIKE ? OR user.user_id LIKE ?)";
