6ddcd8f4dc811ee258b8d9195da2d4a2af284437
plusdojo2026-itohiromi <ito-hiromi-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-29 14:20:11 +0900

    NoticeDao.java

--- source/src/main/java/dao/NoticeDao.java
+++ source/src/main/java/dao/NoticeDao.java
@@ -23,14 +23,14 @@ public class NoticeDao {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する　アップロードの際にコメントアウトをつける
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
+//				+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+//					"root", "password");
 			
-//			アップロードの際にコメントアウトを外す
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
-//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-//					"c1", "c3aXB3a6xd7ursUv");
+			//アップロードの際にコメントアウトを外す
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"c1", "c3aXB3a6xd7ursUv");
 
 			// SQL文を準備する
 			String sql = "SELECT * FROM notice "
@@ -127,14 +127,14 @@ System.out.println(rs.getString("user_name"));
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する　アップロードの際にコメントアウトをつける
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
+//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+//					"root", "password");
 			
 //			アップロードの際にコメントアウトを外す
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
-//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-//					"c1", "c3aXB3a6xd7ursUv");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"c1", "c3aXB3a6xd7ursUv");
 			
 			//日付を取得し変数を格納
 			Calendar cl = Calendar.getInstance();
@@ -224,14 +224,14 @@ System.out.println(rs.getString("user_name"));
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する　アップロードの際にコメントアウトをつける
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
+//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+//					"root", "password");
 			
 //			アップロードの際にコメントアウトを外す
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
-//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-//					"c1", "c3aXB3a6xd7ursUv");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"c1", "c3aXB3a6xd7ursUv");
 			
 			// 日付を取得し変数をを格納
 			Calendar cl = Calendar.getInstance();
@@ -313,14 +313,14 @@ System.out.println(rs.getString("user_name"));
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する　アップロードの際にコメントアウトをつける
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mamoral?"
+//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+//					"root", "password");
 			
 //			アップロードの際にコメントアウトを外す
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
-//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-//					"c1", "c3aXB3a6xd7ursUv");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/c1?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"c1", "c3aXB3a6xd7ursUv");
 
 			// SQL文を準備する
 			String sql = "DELETE FROM Notice WHERE id=?";
